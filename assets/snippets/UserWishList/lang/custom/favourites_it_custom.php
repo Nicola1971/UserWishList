f4780f63dd90ae5c1968db232e8b0b85b660bdc0
Nicola <nlambathakis@gmail.com>
2024-12-01 11:47:30 +0100

    custom language examples

---
+++ assets/snippets/UserWishList/lang/custom/favourites_it_custom.php
@@ -0,0 +1,51 @@
+<?php
+// ---------------------------------------------------------------
+// :: UserWishList
+// ----------------------------------------------------------------
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+
+//UserWishList
+$_UWLlang['exported_on'] = 'Esportato il';
+$_UWLlang['format_not_supported'] = 'Formato non supportato';
+$_UWLlang['wishList_is_empty'] = 'La Lista dei preferiti è vuota';
+$_UWLlang['deleted'] = 'cancellato';
+$_UWLlang['pdf_title'] = 'La mia Lista dei Preferiti';
+$_UWLlang['export_error'] = 'Errore durante l\'esportazione';
+$_UWLlang['error'] = 'Errore';
+$_UWLlang['page'] = 'Pagina';
+$_UWLlang['title'] = 'Titolo';
+$_UWLlang['description'] = 'Descrizione';
+$_UWLlang['URL'] = 'URL';
+$_UWLlang['your_wishList_is_empty'] = 'La tua Lista dei Preferiti è vuota';
+$_UWLlang['export_wishList'] = 'Esporta Lista';
+$_UWLlang['saved_elements'] = 'Elementi salvati';
+
+//AddToWishList
+$_UWLlang['btnAddText'] = 'Aggiungi alla lista dei Preferiti';
+$_UWLlang['btnAddAlt'] = 'Aggiungi alla lista dei Preferiti';
+$_UWLlang['btnAlreadyText'] = 'Già salvato nei Preferiti';
+$_UWLlang['btnAlreadyAlt'] = 'Elemento già presente nei Preferiti';
+$_UWLlang['ToNotLoggedTpl'] = '<p class="text-muted">Effettua il login per aggiungere ai Preferiti</p>';
+$_UWLlang['btnNotLoggedAlt'] = 'Effettua il login per aggiungere ai Preferiti';
+$_UWLlang['add_to_wishList'] = 'Aggiungi ai Preferiti';
+$_UWLlang['added_to_wishList'] = 'Aggiunto ai Preferiti';
+$_UWLlang['already_in_wishList'] = 'Già presente nei Preferiti';
+$_UWLlang['counter_format'] = 'Salvato da %d utenti';
+$_UWLlang['toast_success'] = 'Aggiunto ai Preferiti';
+$_UWLlang['toast_error'] = 'Errore durante l\'aggiunta';
+$_UWLlang['counter_update_error'] = 'Errore nell\'aggiornamento dei contatori';
+
+//RemoveFromWishList
+$_UWLlang['btnRemoveText'] = 'Rimuovi dai Preferiti';
+$_UWLlang['btnNotInText'] = 'Non salvato nei Preferiti';
+$_UWLlang['removed_from_wishList'] = 'Rimosso dai Preferiti';
+$_UWLlang['item_notin_wishList'] = 'Elemento non presente nei Preferiti';
+
+//MostWished
+$_UWLlang['tv_notfound'] = 'Template Variabile non trovata';
