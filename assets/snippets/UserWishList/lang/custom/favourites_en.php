e9c8d494ca7cabed96bc7fc76789f494b62446d9
Nicola <nlambathakis@gmail.com>
2024-12-07 16:59:00 +0100

    fix custom lang

--- assets/snippets/UserWishList/lang/custom/favourites_en.php
+++ assets/snippets/UserWishList/lang/custom/favourites_en.php
@@ -11,43 +11,42 @@
 //
 
 //UserWishList
-$_UWLlang['exported_on'] = 'Exported on';
-$_UWLlang['format_not_supported'] = 'Format not supported';
-$_UWLlang['wishList_is_empty'] = 'The Favourites list is empty';
-$_UWLlang['deleted'] = 'deleted';
-$_UWLlang['pdf_title'] = 'My Favourites List';
-$_UWLlang['export_error'] = 'Error during export';
-$_UWLlang['error'] = 'Error';
-$_UWLlang['page'] = 'Page';
-$_UWLlang['title'] = 'Title';
-$_UWLlang['description'] = 'Description';
-$_UWLlang['URL'] = 'URL';
-$_UWLlang['your_wishList_is_empty'] = 'Your Favourites list is empty';
-$_UWLlang['export_wishList'] = 'Export Favourites';
-$_UWLlang['saved_elements'] = 'Saved items';
-
-//AddToWishList
-$_UWLlang['btnAddText'] = 'Add to Favourites';
-$_UWLlang['btnAddAlt'] = 'Add to Favourites';
-$_UWLlang['btnAlreadyText'] = 'Already saved in Favourites';
-$_UWLlang['btnAlreadyAlt'] = 'Item already in Favourites';
-$_UWLlang['ToNotLoggedTpl'] = '<p class="text-muted">Log in to add to Favourites</p>';
-$_UWLlang['btnNotLoggedAlt'] = 'Log in to add to Favourites';
-$_UWLlang['add_to_wishList'] = 'Add to Favourites';
-$_UWLlang['added_to_wishList'] = 'Added to Favourites';
-$_UWLlang['already_in_wishList'] = 'Already in Favourites';
-$_UWLlang['counter_format'] = 'Saved by %d users';
-$_UWLlang['toast_success'] = 'Added to Favourites';
-$_UWLlang['toast_error'] = 'Error while adding';
-$_UWLlang['counter_update_error'] = 'Error updating counters';
-
-//RemoveFromWishList
-$_UWLlang['btnRemoveText'] = 'Remove from Favourites';
-$_UWLlang['btnNotInText'] = 'Not saved in Favourites';
-$_UWLlang['removed_from_wishList'] = 'Removed from Favourites';
-$_UWLlang['item_notin_wishList'] = 'Item not in Favourites';
-$_UWLlang['btnRemoveAlt'] = 'Remove from Favourites';
-$_UWLlang['btnNotInAlt'] = 'Not saved in Favorites';
-
-//MostWished
-$_UWLlang['tv_notfound'] = 'Template Variable not found';
+$_UWLlang = array(
+    'exported_on' => 'Exported on',
+    'format_not_supported' => 'Format not supported',
+    'wishList_is_empty' => 'The Wish List is empty',
+    'deleted' => 'Deleted',
+    'pdf_title' => 'My Wish List',
+    'export_error' => 'Error during export',
+    'error' => 'Error',
+    'page' => 'Page',
+    'title' => 'Title',
+    'description' => 'Description',
+    'URL' => 'URL',
+    'your_wishList_is_empty' => 'Your Wish List is empty',
+    'export_wishList' => 'Export Wish List',
+    'saved_elements' => 'Saved items',
+    //AddToWishList
+    'btnAddText' => 'Add to Wish List',
+    'btnAddAlt' => 'Add to Wish List',
+    'btnAlreadyText' => 'Already saved in Wish List',
+    'btnAlreadyAlt' => 'Item already in Wish List',
+    'ToNotLoggedTpl' => '<p class="text-muted">Log in to add to Wish List</p>',
+    'btnNotLoggedAlt' => 'Log in to add to Wish List',
+    'add_to_wishList' => 'Add to Wish List',
+    'added_to_wishList' => 'Added to Wish List',
+    'already_in_wishList' => 'Already in Wish List',
+    'counter_format' => 'Saved by %d users',
+    'toast_success' => 'Added to Wish List',
+    'toast_error' => 'Error while adding',
+    'counter_update_error' => 'Error updating counters',
+    //RemoveFromWishList
+    'btnRemoveText' => 'Remove from Wish List',
+    'btnNotInText' => 'Not saved in Wish List',
+    'removed_from_wishList' => 'Removed from Wish List',
+    'item_notin_wishList' => 'Item not in Wish List',
+    'btnRemoveAlt' => 'Remove from Wish List',
+    'btnNotInAlt' => 'Not in Wish List',
+    //MostWished
+    'tv_notfound' => 'Template Variable not found'
+);
