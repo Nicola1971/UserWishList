a587527e229723a531c6a4b26de3e00d19a052c7
Nicola <nlambathakis@gmail.com>
2024-12-10 20:03:50 +0100

    uk language

---
+++ assets/snippets/UserWishList/lang/uk.php
@@ -0,0 +1,38 @@
+<?php
+// ---------------------------------------------------------------
+// :: UserWishList
+// ----------------------------------------------------------------
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+
+//UserWishList
+$_UWLlang['wishList_is_empty'] = 'Список бажань порожній';
+$_UWLlang['deleted'] = 'видалено';
+$_UWLlang['error'] = 'Помилка';
+$_UWLlang['URL'] = 'URL';
+$_UWLlang['your_wishList_is_empty'] = 'Ваш список бажань порожній';
+$_UWLlang['saved_elements'] = 'Збережені елементи';
+$_UWLlang['btnRemoveText'] = 'Видалити зі списку бажань';
+$_UWLlang['removed_from_wishList'] = 'Видалено зі списку бажань';
+$_UWLlang['btnRemoveAlt'] = 'Видалити зі списку бажань';
+//AddToWishList
+$_UWLlang['btnAddText'] = 'Додати до списку бажань';
+$_UWLlang['btnAddAlt'] = 'Додати до списку бажань';
+$_UWLlang['btnAlreadyText'] = 'Вже у списку бажань';
+$_UWLlang['btnAlreadyAlt'] = 'Елемент вже є у списку бажань';
+$_UWLlang['ToNotLoggedTpl'] = 'Увійдіть, щоб додати до вашого списку бажань';
+$_UWLlang['btnNotLoggedAlt'] = 'Увійдіть, щоб додати до списку бажань';
+$_UWLlang['add_to_wishList'] = 'Додати до списку бажань';
+$_UWLlang['added_to_wishList'] = 'Додано до списку бажань';
+$_UWLlang['already_in_wishList'] = 'Вже у списку бажань';
+$_UWLlang['counter_format'] = 'Спостерігається %d користувачами';
+$_UWLlang['toast_success'] = 'Додано до списку бажань';
+$_UWLlang['toast_error'] = 'Помилка під час додавання';
+$_UWLlang['counter_update_error'] = 'Помилка оновлення лічильників';
+//MostWished
+$_UWLlang['tv_notfound'] = 'Шаблонна змінна не знайдена';
